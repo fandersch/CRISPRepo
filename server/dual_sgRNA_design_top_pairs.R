80139fdea7f754a10fa9278c0d8a19e1ec7f1a83
Florian Andersch <florian.andersch@imp.ac.at>
2024-10-21 18:27:35 +0200

    fix bugs top dual pairs #2

--- server/dual_sgRNA_design_top_pairs.R
+++ server/dual_sgRNA_design_top_pairs.R
@@ -67,7 +67,7 @@ dualSgRNAsTopCombinationsTable <- reactive({
   DBI::dbDisconnect(con_sgRNAs)
   
   sgRNAs %>%
-    rowwise() %>%
+    dplyr::rowwise() %>%
     dplyr::rename(first_sgRNA_23mer = first_sgRNA, matching_sgRNA_23mer=matching_sgRNA) %>%
     dplyr::mutate(name_first_fwd=paste0(Symbol, "_", EntrezID, "_", first_sgRNA_final_rank, "_fwd"), name_match_fwd=paste0(Symbol, "_", EntrezID, "_", matching_sgRNA_final_rank, "_fwd"),
                   seq_first_fwd=paste0("cacc", first_sgRNA_mature), seq_match_fwd=paste0("cttgttt", matching_sgRNA_mature, "gttta"),
@@ -75,7 +75,8 @@ dualSgRNAsTopCombinationsTable <- reactive({
                   seq_first_rev=paste0("aaac", as.character(reverseComplement(DNAString(as.character(first_sgRNA_mature))))),	seq_match_rev=paste0("ctcttaaac", as.character(reverseComplement(DNAString(as.character(matching_sgRNA_mature)))), "aaa")
                   ) %>%
     mutate_at(c("first_sgRNA_MHstrength", "matching_sgRNA_MHStrength", "score_penalty", "first_guide_rank_score", "second_guide_rank_score", "score_combined"),  round, 2) %>%
-    ungroup() %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(EntrezID) %>%
     filter(dplyr::row_number() <= input$dualSgRNAsTopCombinationsnOutput)
 })
 
